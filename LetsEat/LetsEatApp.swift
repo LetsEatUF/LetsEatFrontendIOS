6761f3124bfaf009dd6ba2b589a41a52d5c9454d
Elijah Springer <springer_elijah@yahoo.com>
2021-01-29 21:24:40 -0500

    Initial Commit

---
+++ LetsEat/LetsEatApp.swift
@@ -0,0 +1,17 @@
+//
+//  LetsEatApp.swift
+//  LetsEat
+//
+//  Created by Elijah Springer on 1/29/21.
+//
+
+import SwiftUI
+
+@main
+struct LetsEatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
